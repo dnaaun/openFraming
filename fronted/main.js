25a85c97c1e55137643cf8c6fa5f17df688723df
Mona Jalal <jalal@bu.edu>
2020-06-12 09:58:07 -0400

    added a basic jQuery connected to index.html for echoing the message in textarea'

---
+++ fronted/main.js
@@ -0,0 +1,31 @@
+/* submit button
+
+$(function () {
+    $('form').on('submit', function (e) {
+
+        $.ajax(
+                        {
+                            url:'/submitData',
+                            data: $('form').serialize(),
+                            type: 'POST',
+                            success: function (data) {
+                                alert("Submitted");
+                                console.log(data);
+                        },
+                        error: function (error){
+                            console.log(error)
+                        }
+                    });
+    });
+
+});
+
+ */
+
+$(function(){
+    $("form").on('submit',  function(e){
+       // alert("Submitted");
+        var data = $('#mytextarea').val();
+        alert(data);
+    });
+});
