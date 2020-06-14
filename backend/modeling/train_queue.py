b9806454fcbb21e0f572d65d84c09421695fb30c
Alyssa Smith <asmithh99@gmail.com>
2020-06-14 18:53:25 -0400

    adding rq to training queue, which greatly simplifies things

--- backend/modeling/train_queue.py
+++ backend/modeling/train_queue.py
@@ -2,42 +2,20 @@ from concurrent.futures import ThreadPoolExecutor
 import pickle
 
 import redis
+from rq import Queue
 
 from classifier import ClassifierModel
 
 
-TRAINING_QUEUE_NAME = 'TRAIN'
-PROCESSES = 2
-
 class ModelScheduler(object):
 	def __init__(self):
 		self.my_redis = redis.Redis()
-		self.queue_name = TRAINING_QUEUE_NAME
+		self.queue = Queue(connection=self.my_redis)
 	def add_training_process(self, labels, model_path, data_dir):
 		pickle_data = {'labels': labels, 'model_path': model_path, 'data_dir': data_dir}
-		self.my_redis.lpush(self.queue_name, pickle.dumps(pickle_data))
-
+		# self.my_redis.lpush(self.queue_name, pickle.dumps(pickle_data))
+		self.queue.enqueue(do_train, pickle_data)
 
 def do_train(pickle_data):
 	classifier_model = ClassifierModel(pickle_data['labels'], pickle_data['model_path'], pickle_data['data_dir'], './')
-	print('beginning training')
 	classifier_model.train()
-	print('training happened')
-
-
-class ModelTrainer(object):
-	def __init__(self):
-		self.training_queue_name = TRAINING_QUEUE_NAME
-		self.my_redis = redis.Redis()
-
-	def do(self):
-		with ThreadPoolExecutor(max_workers=4) as e:
-			while True:
-				if self.my_redis.llen(self.training_queue_name) > 0:
-					_, pickled = self.my_redis.brpop(self.training_queue_name)
-					pickle_data = pickle.loads(pickled)
-					e.submit(do_train, pickle_data)
-
-
-# m = ModelTrainer()
-# m.do()
