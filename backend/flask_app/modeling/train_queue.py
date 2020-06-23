5a541a523af9167da58ca6d753087322bc6d3a36
David Assefa Tofu <davidat@bu.edu>
2020-06-23 14:24:13 -0400

    cache-dir passed to scheduler

--- backend/flask_app/modeling/train_queue.py
+++ backend/flask_app/modeling/train_queue.py
@@ -12,19 +12,23 @@ class ModelScheduler(object):
         self.queue = Queue(connection=self.my_redis)
 
     def add_training_process(
-        self, labels: T.List[str], model_path: str, data_dir: str
+        self, labels: T.List[str], model_path: str, data_dir: str, cache_dir: str
     ) -> None:
-        pickle_data = {"labels": labels, "model_path": model_path, "data_dir": data_dir}
+        pickle_data = {
+            "labels": labels,
+            "model_path": model_path,
+            "data_dir": data_dir,
+            "cache_dir": cache_dir,
+        }
         # self.my_redis.lpush(self.queue_name, pickle.dumps(pickle_data))
         self.queue.enqueue(do_train, pickle_data)
 
 
 def do_train(pickle_data: T.Dict[str, T.Any]) -> None:
     classifier_model = ClassifierModel(
-        # TODO: Make the cache_dir argument (which is a "./" right now) a variable
         pickle_data["labels"],
         pickle_data["model_path"],
         pickle_data["data_dir"],
-        "./",
+        pickle_data["cache_dir"],
     )
     classifier_model.train()
