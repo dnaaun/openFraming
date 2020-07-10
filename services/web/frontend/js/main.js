dd06d2f17f222402e1a72fa4bcb7fcbd7f0f8e47
Edward Edberg <edward.edberg.halim@gmail.com>
2020-07-10 23:11:23 +0700

    edit page and make connection with api

--- services/web/frontend/js/main.js
+++ services/web/frontend/js/main.js
@@ -2,11 +2,257 @@ $(".other-option").hide();
 
 $(".form-check-inline").on("click", function(){
    $(".other-option").fadeOut();
+   $('#other_text').val('')
+   $('#category_names').val('')
 });
 
 $(".other-policy").on("click", function(){
    $(".other-option").fadeIn()();
 });
+
+
+var stateClassifier_id='';
+
+
+// GET POST
+//
+
+const endpoint = "http://ec2-3-90-135-165.compute-1.amazonaws.com/api/"
+
+async function getFraming() {
+	// console.log("asdfdas");
+	var endpointGET = endpoint + 'classifiers/'
+	await axios
+	   .get(endpointGET)
+	   .then(res => console.log(res))
+	   .catch(err => console.error(err));
+	
+ }
+   
+async function postFraming() {
+	console.log("POST");
+	// let name = $("input[name='policyissue']:checked").val();
+
+
+
+	// let name = document.getElementById("other_text").value;
+	let name = $("input[name='policyissue']:checked").val();
+
+	if (name === "gunviolence") {
+		console.log("gunviolence")
+		var arrayCategoryNames = ["gun_topic1", "gun_topic2"]
+		console.log(arrayCategoryNames)
+	} else if (name === "immigration") {
+		console.log("immigration")
+		var arrayCategoryNames = ["immigration1", "immigration2"]
+		console.log(arrayCategoryNames)
+	} else if (name === "tobocco") {
+		console.log("tobocco")
+		var arrayCategoryNames = ["tobocco1", "tobocco2"]
+		console.log(arrayCategoryNames)
+	} else if (name === "samesexmarriage") {
+		console.log("samesexmarriage")
+		var arrayCategoryNames = ["samesexmarriage1", "samesexmarriage2"]
+		console.log(arrayCategoryNames)
+	} else if (name === "covid19") {
+		console.log("covid19")
+		var arrayCategoryNames = ["covid19_1", "covid19_2"]
+		console.log(arrayCategoryNames)
+	} else if (name === "climatechange") {
+		console.log("climatechange")
+		var arrayCategoryNames = ["climatechange1", "climatechange2"]
+		console.log(arrayCategoryNames)
+	} else if (name === "other") {
+		console.log("other")
+		var stringCategoryNames = document.getElementById("category_names").value;
+		var arrayCategoryNames = stringCategoryNames.split(',');
+		console.log(arrayCategoryNames)
+	} 
+
+
+	// console.log(name);
+	
+
+	await axios
+		.post('http://ec2-3-90-135-165.compute-1.amazonaws.com/api/classifiers/', {
+			name: name,
+			category_names: arrayCategoryNames
+		})
+		.then(res => {
+			var classifier_id = res.data["classifier_id"]
+			console.log(classifier_id)
+			stateClassifier_id = classifier_id
+			// return classifier_id
+		})
+		.catch(err => console.error(err));
+
+	// var classifier_id = res.data["classifier_id"]
+	// console.log(classifier_id);
+	// callback();
+	
+}
+
+//
+//GET /classifiers/<classifier_id:int>
+//
+function checkClassifier(classifier_id) {
+	var endpointGET = endpoint + 'classifiers/' + classifier_id
+	axios
+	   .get(endpointGET)
+	   .then(res => console.log(res.data))
+	   .catch(err => console.error(err));
+	
+}
+
+
+async function upTrainingFile() {
+
+	// http://ec2-3-90-135-165.compute-1.amazonaws.com/api/classifiers/
+	var endpointUploadTraining = endpoint + 'classifiers/' + stateClassifier_id + '/training/file'
+	// var file = $('#annotatedsamplefile2');
+	// let formData = new FormData(file[0]);
+	// // console.log(fileName.val());
+	// console.log(formData);
+
+	var formData = new FormData();
+	var imagefile = document.querySelector('#annotatedsamplefile1');
+	formData.append("image", imagefile.files[0]);
+	console.log(imagefile.files[0])
+	// var form = $('#annotatedsamplefile2');
+	// let formData = new FormData(form[0]);
+	// axios.post('upload_file', formData, {
+	// 	headers: {
+	// 		"Access-Control-Allow-Origin": "*"
+	// 	}
+	// })
+
+	await axios
+		.post(endpointUploadTraining, formData, {
+			headers: {
+				'Content-Type': 'multipart/form-data'
+			}
+		})
+	    .then(res => {
+	        console.log({res});
+		})
+		.catch(err => {
+	        console.error({err});
+	    });
+}
+
+async function upTestingFile() {
+
+
+	var endpointUploadTraining = endpoint + 'classifiers/' + stateClassifier_id + '/training/file'
+
+
+	var formData = new FormData();
+	var imagefile = document.querySelector('#annotatedsamplefile2');
+	formData.append("image", imagefile.files[0]);
+	console.log(imagefile.files[0])
+
+	await axios
+		.post(endpointUploadTraining, formData, {
+			headers: {
+				'Content-Type': 'multipart/form-data'
+			}
+		})
+	    .then(res => {
+	        console.log({res});
+		})
+		.catch(err => {
+	        console.error({err});
+	    });
+}
+
+var fileName = $('input[type="file"]').change(function(e){
+	var fileName = e.target.files[0].name;
+	console.log('The file "' + fileName +  '" has been selected.');
+}); // collect file name after uploaded
+
+// var fileName = $('input[type="file"]').change(function(e){
+// 	var fileName = e.target.files[0].name;
+// 	console.log('The file "' + fileName +  '" has been selected.');
+// }); // collect file name after uploaded
+
+// var fileName = $('input[type="file"]').change(function(e){
+// 	var fileName = e.target.files[0].name;
+// 	console.log('The file "' + fileName +  '" has been selected.');
+// });
+
+// async.parallel([
+//     function(){ ... },
+//     function(){ ... }
+// ], callback);
+
+$('#performAnalysis').click(async function(){
+	// $.when($.when(step1()).then(step2)).then(step3);
+	// console.log($('#email').val()); // take email
+	 // Creates a classifier.
+	 // List all classifiers.
+	 // Get details about one classifier.
+	// var class_id = postFraming();
+	// getFraming();
+	// console.log(class_id);
+	// postFraming(getFraming);
+	// getFraming();
+	// getFraming(postFraming());
+	
+	await postFraming();
+	await getFraming();
+	await upTrainingFile();
+	await upTestingFile();
+	// checkClassifier(id);
+
+	// async function init() {
+		
+	// 	await postFraming();
+	// 	await getFraming();
+	// 	// checkClassifier();
+	// }
+
+	// init();
+	// upTrainingFile();
+
+
+	// Get details about one classifier.
+	// GET /classifiers/<classifier_id:int></classifier_id>
+
+	// Upload training data for a classifier and start training.
+	// POST /classifiers/<classifier_id:int>/training/file
+
+	// Lists all test sets for a classifier.
+	// GET /classifiers/<classifier_id:int>/test_sets/
+
+	// Create a test set.
+	// POST /classifiers/<classifier_id:int>/test_sets/
+
+	// Get details about one test set.
+	// GET /classifiers/<classifier_id:int>/test_sets/<test_set_id:int>
+
+
+	// Upload test set and start inference.
+	// POST /classifiers/<classifier_id:int>/test_sets/<test_set_id:int>/file
+
+	// Download predictions on test set.
+	// GET /classifiers/<classifier_id:int>/test_sets/<test_set_id:int>/predictions
+
+
+
+	
+
+
+	
+
+
+	
+
+ });
+ 
+
+
+
+
 /* submit button
 
 $(function () {
